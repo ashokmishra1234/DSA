f3db684ad3a83b8736e20a9c2fdee39609167615
ASHOK MISHRA <ashokmishra1709@gmail.com>
2026-07-12 23:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (57.77%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int> left_max(n,0),right_max(n,0);
-        int curr_max=0;
-        //right_max
-        for(int i=n-1;i>=0;i--){
-            if(curr_max > height[i])right_max[i]=curr_max;
-            curr_max=max(curr_max,height[i]);
-        }
-        curr_max=0;
-        //left max
-        for(int i=0;i<n;i++){
-            if(curr_max > height[i]) left_max[i]=curr_max;
-            curr_max=max(curr_max,height[i]);
-        }
+    int trap(vector<int>& arr) {
+        int n=arr.size();
+        int l=0,r=n-1,lmax=0,rmax=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            int lh=left_max[i];
-            int rh=right_max[i];
-            int ah=min(lh,rh);
-            if(ah) ans+=(ah-height[i]);
+        while(l < r){
+            if(arr[l] < arr[r]){
+                if(arr[l] < lmax){
+                    ans+=(lmax-arr[l]);
+                   
+                }else{
+                    lmax=max(lmax,arr[l]);
+                }
+                l+=1;
+            }else{
+                if(arr[r] < rmax){
+                    ans+=(rmax-arr[r]);
+                }else rmax=max(rmax,arr[r]);
+                r--;
+            }
         }
         return ans;
     }
