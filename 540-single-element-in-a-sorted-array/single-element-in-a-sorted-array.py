ac18a537733d8564bc3cebfc89c8db3f50a0ec01
ASHOK MISHRA <ashokmishra1709@gmail.com>
2026-08-05 12:03:54 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (90.64%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -16,14 +16,14 @@ class Solution:
                 l_ele=mid+1
                 r_ele=n-l_ele
                 if l_ele%2:
-                    r=mid-1
+                    r=mid-2
                 else:
                     l=mid+1
             else:
                 r_ele=n-mid
                 l_ele=n-r_ele
                 if r_ele % 2:
-                    l=mid+1
+                    l=mid+2
                 else:
                     r=mid-1
         return -1
