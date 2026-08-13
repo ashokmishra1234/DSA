7116f011405a44d26451194da163de56cd9aa595
ASHOK MISHRA <ashokmishra1709@gmail.com>
2026-08-13 20:16:15 +0530

    Time: 7 ms (93.46%) | Memory: 12.6 MB (51.01%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -7,20 +7,18 @@ public:
         for(int i=0;i<n;i++) parent[i]=i;
         int maxLen=0,maxInd=0;
         for(int i=0;i<n;i++){
-            int temp=1;
             for(int j=0;j<i;j++){
                 if(nums[i]%nums[j]==0){
-                    if(len[i]+len[j] > temp){
-                        temp=len[i]+len[j];
+                    if(len[j] + 1 > len[i]){
+                        len[i]=len[i]+len[j];
                         parent[i]=j;
+                        if(len[i] > maxLen){
+                            maxLen=len[i];
+                            maxInd=i;
+                        }
                     }
 
                 }
-            }
-            len[i]=temp;
-            if(len[i] > maxLen){
-                maxLen=len[i];
-                maxInd=i;
             }
         }
         vector<int> ans;
