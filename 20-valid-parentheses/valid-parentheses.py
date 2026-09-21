3dd5c877f9bbeffc2185075a378ac02150ca190f
ASHOK MISHRA <ashokmishra1709@gmail.com>
2026-09-21 11:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.48%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for ch in s:
+            if ch=='(' or ch=='{' or ch=='[':
+                stack.append(ch)
+            else:
+                if len(stack) < 1:
+                    return False
+                top=stack.pop()
+                if (ch==')' and top!='(') or (ch=='}' and top!='{') or (ch==']' and top!='['):
+                    return False
+        if len(stack) > 0:
+            return False
+        return True
